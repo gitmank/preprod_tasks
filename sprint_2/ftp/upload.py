517db854087e0cf64093cd05b964c5c7b51d111d
Manomay Kagalkar <manomay111@gmail.com>
2024-09-19 18:26:54 +0530

    minor changes to file paths

--- sprint_2/ftp/upload.py
+++ sprint_2/ftp/upload.py
@@ -7,8 +7,8 @@ import pandas as pd # use dataframes
 FTP_SERVER='188.245.121.87'
 FTP_USER='preprod'
 FTP_PASSWORD='preprod' # UNSAFE - replace hardcoded credentials
-FILE_PATH='../MOCK_DATA.csv'
-UPLOAD_NAME='test.csv'
+FILE_PATH='sprint_2/MOCK_DATA.csv'
+UPLOAD_NAME='preprod.csv'
 
 # this function uploads a file to the ftp server
 def upload_file(path):
